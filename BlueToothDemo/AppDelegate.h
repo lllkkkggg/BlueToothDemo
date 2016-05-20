692b9f5bfd4276eb777497a929fbdc15d17a858c
iosOne <iosone@iosonedeimac.local>
2016-05-20 11:42:48 +0800

    Initial Commit

---
+++ BlueToothDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlueToothDemo
+//
+//  Created by iosOne on 16/5/20.
+//  Copyright © 2016年 吕盼举. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BlueToothDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlueToothDemo
+//
+//  Created by iosOne on 16/5/20.
+//  Copyright © 2016年 吕盼举. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
